8e85440762008dd2fe5a1968276f90ca63aa8e25
PWcislo26 <92379337+PWcislo26@users.noreply.github.com>
2022-03-08 13:25:00 +0100

    Update lab1.py
    updated naming convention

--- lab1.py
+++ lab1.py
@@ -3,30 +3,30 @@ import re
 # zad 1
 text1 = "Dzisiaj mamy 4 stopnie na plusie, 1 marca 2022 roku"
 
-removeNumbers = re.sub("[0-9]+", "", text1)
-print(removeNumbers)
+remove_numbers = re.sub("[0-9]+", "", text1)
+print(remove_numbers)
 
 text2 = "<div><h2>Header</h2> <p>article<b>strong text</b> <a href="">link</a> </p></div>"
-removeHTML = re.sub("[^>]", "", text2)
-print(removeHTML)
+remove_html = re.sub("[^>]", "", text2)
+print(remove_html)
 
 text3 = """Lorem ipsum dolor sit amet, consectetur; adipiscing elit.
 Sed eget mattis sem. Mauris egestas erat quam, ut faucibus eros congue et. In
 blandit, mi eu porta; lobortis, tortor nisl facilisis leo, at tristique augue risus
 eu risus."""
-removePunctuaction = re.sub(r"[^\w\s]", '', text3)
-print(removePunctuaction)
+remove_punctuaction = re.sub(r"[^\w\s]", '', text3)
+print(remove_punctuaction)
 
 # zad 2
 text4 = """Lorem ipsum dolor
 sit amet, consectetur adipiscing elit. Sed #texting eget mattis sem. Mauris #frasista
 egestas erat #tweetext quam, ut faucibus eros #frasier congue et. In blandit, mi eu porta
 lobortis, tortor nisl facilisis leo, at tristique #frasistas augue risus eu risus."""
-findHashtags = re.findall(r"#[a-z0-9_]+", text4)
-print(findHashtags)
+find_hashtags = re.findall(r"#[a-z0-9_]+", text4)
+print(find_hashtags)
 
 # zad 3
 
 text5 = """:) ;) ;( :> :< ;< :-) ;-)"""
-findEmotes = re.findall(r"[:|;][-]?[\)|\(|<|>]", text5)
-print(findEmotes)
+find_emotes = re.findall(r"[:|;][-]?[\)|\(|<|>]", text5)
+print(find_emotes)
