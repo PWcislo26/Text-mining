0fd94e74f3274315af86e791decfc9ddec8acca2
Piotr <92379337+PWcislo26@users.noreply.github.com>
2022-03-08 19:40:18 +0100

    Stemming function

--- stemming.py
+++ stemming.py
@@ -4,23 +4,37 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize, sent_tokenize
 
-eee = """  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
- specimen book. It has survived not only five centuries, but also the leap into electronic  """
+eee = """In Guam, she lAIDd in the sun 323321 and ate mangoes all day.
+She got hiccups and couldn't get rid 22of them for three hours.
+Let's all just takE a mOMent to breathe, please!"""
 
 
 def text_cleanup(text: str) -> str:
-    remove_numbers = re.sub("[0-9]+", "", text)
-    remove_html = re.sub("[^>]", "", remove_numbers)
-    remove_punctuation = re.sub(r"[^\w\s]", '', remove_html)
-    remove_whitespaces = re.sub(r"^\s+|\s+$",'', remove_punctuation)
-    print(remove_numbers)
+    tmp = ""
+    for char in text:
+        if char.isalpha():
+            tmp += char.lower()
+        else:
+            tmp += char
+    remove_numbers = re.sub("[0-9]+", "", tmp)
+    remove_punctuation = re.sub(r"[^\w\s]", '', remove_numbers)
+    remove_whitespaces = re.sub(r"^\s+|\s+$", '', remove_punctuation)
+
+    return remove_whitespaces
 
 
-def stemming(text:str) -> list:
-    pass
+def stemming(text: str) -> list:
+    ps = PorterStemmer()
+    non_stopwords = []
+    stemming_output = []
+    stop_list = set(stopwords.words("english"))
+    words = word_tokenize(text)
+    for word in words:
+        if word not in stop_list:
+            non_stopwords.append(word)
+    for word in non_stopwords:
+        stemming_output.append(ps.stem(word))
+    return stemming_output
 
 
-text_cleanup(eee)
-remove_html = re.sub("[^>]", "", eee)
-print(remove_html)
+print(stemming(text_cleanup(eee)))
