781c59c61a43d57836bf6eda8b123a3a29974c3b
PWcislo26 <piotr.wcislo@edu.uekat.pl>
2022-04-12 10:18:49 +0200

    Added dataframes, moved tool functions to separate file

--- lab_5.py
+++ lab_5.py
@@ -1,56 +1,54 @@
-import re
-
 import numpy
 import pandas
-from nltk import PorterStemmer
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction.text import CountVectorizer
+import matplotlib.pyplot as plt
 
-
-def cleanup_text(text: str) -> str:
-    emoticons = re.findall(r'[:|;][-]?[)|(|<>]', text)
-    text_low = text.lower()
-    text_without_number = re.sub(r'\d', '', text_low)
-    text_without_html = re.sub(r'<.*?>', '', text_without_number)
-    text_without_punc_marks = re.sub(r'\W(?<!\s)', '', text_without_html)
-    text_without_white_space = text_without_punc_marks.strip()
-    text_done = text_without_white_space + ' '.join(emoticons)
-    return text_done
-
-
-def delete_stop_words(text: str) -> list:
-    stop_words = stopwords.words("english")
-    return [w for w in text if not w.lower() in stop_words]
-
-
-def stemming(word: str) -> str:
-    ps = PorterStemmer()
-    return ps.stem(word)
-
-
-def text_tokenizer(text: str):
-    cleaned = cleanup_text(text)
-    tokens = word_tokenize(cleaned)
-    without_stopwords = delete_stop_words(tokens)
-
-    return [stemming(w) for w in without_stopwords if len(w) > 3]
+from tools import text_tokenizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from tabulate import tabulate
 
 
 def tokens():
-    """print 10 most occuring tokens from sample text"""
+    """returns a dataframe of 10 most occurring tokens """
     reader = pandas.read_csv('True.csv', usecols=['title', 'text'], header=0)
-    sample = reader['title'][:10]
-    vectorizer = CountVectorizer(tokenizer=text_tokenizer)
-    x_transform = vectorizer.fit_transform(sample)
-    column_names = vectorizer.get_feature_names_out()  # token string representation
+    sample = reader['text'][:10]
+    vectorizer_tokens = CountVectorizer(tokenizer=text_tokenizer)
+    x_transform = vectorizer_tokens.fit_transform(sample)
+    column_names = vectorizer_tokens.get_feature_names_out()  # token string representation
     array = x_transform.toarray()  # array of tokens
     column_sums = numpy.sum(array, axis=0)  # token occurrences
     most_occurring_idx = numpy.argpartition(column_sums, -10)[-10:]  # 10 most occurring token indexes
-    most_occurring = []
+    most_occurring_tokens = []
+    token_occurrences = []
+
     for index in numpy.nditer(most_occurring_idx):
-        most_occurring.append(column_names[index])
-    print(most_occurring)
+        most_occurring_tokens.append(column_names[index])
+        token_occurrences.append(column_sums[index])
+
+    data = {'Tokens': most_occurring_tokens, 'Appearances': token_occurrences}
+    df = pandas.DataFrame(data)
+    return df
+
+
+def token_weights():
+    """returns a dataframe of 10 tokens with highest combined weights"""
+    reader = pandas.read_csv('True.csv', usecols=['title', 'text'], header=0)
+    sample = reader['text'][:10]
+    vectorizer = TfidfVectorizer(tokenizer=text_tokenizer)
+    x_transform = vectorizer.fit_transform(sample)
+    column_names = vectorizer.get_feature_names_out()  # token string representation
+    array = x_transform.toarray()  # array of tokens
+    token_column_sums = numpy.sum(array, axis=0)  # summed token weights
+    highest_weight_indexes = numpy.argpartition(token_column_sums, -10)[-10:]  # 10 token indexes with highest summed weight
+    highest_weight_tokens = []  # 10 highest token weight names
+    highest_weight = []  # 10 highest weights
+
+    for index in numpy.nditer(highest_weight_indexes):
+        highest_weight_tokens.append((column_names[index]))
+        highest_weight.append(token_column_sums[index])
+
+    data = {'Tokens': highest_weight_tokens, 'Weights': highest_weight}
+    df = pandas.DataFrame(data)
+    return df
 
 
 def documents():
@@ -65,9 +63,22 @@ def documents():
     most_tokens_idx = numpy.argpartition(row_sums, -10)[-10:]  # row indexes with most tokens
     print(most_tokens_idx)
     most_occurring = []
-    for index in numpy.nditer(most_tokens_idx):  # prints documents with most tokens
+
+    for index in numpy.nditer(most_tokens_idx):
         most_occurring.append(sample[index])
     print(most_occurring)
 
 
-documents()
+def main():
+    data_tokens = tokens()
+    data_tokens.plot(kind='barh', x='Tokens', y='Appearances')
+    print(tabulate(data_tokens, headers='keys', tablefmt='psql'))
+    plt.show()
+
+    data_weights = token_weights()
+    data_weights.plot(kind='barh', x='Tokens', y='Weights')
+    print(tabulate(data_weights, headers='keys', tablefmt='psql'))
+    plt.show()
+
+
+main()
