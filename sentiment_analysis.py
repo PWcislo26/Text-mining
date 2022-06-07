fff32ff2632bb2f6d724ce9215038227c9dc4687
PWcislo26 <piotr.wcislo@edu.uekat.pl>
2022-06-07 10:10:23 +0200

    kolokwium

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,23 +1,22 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import texttable
 
 from util import text_tokenizer, generate_dataframe, generate_wordclouds, show_wordclouds, show_plots
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import classification_report
 from tabulate import tabulate
 
 
-def token_weights(df: pd.DataFrame):
+def token_weights_positive(df: pd.DataFrame):
     """Show 10 highest token weights for positive reviews"""
-    df_pos = df[df['rating'] > 2]
+    df_pos = df[df['airline_sentiment'] == "positive"]
     vectorizer = TfidfVectorizer(tokenizer=text_tokenizer)
-    x_transform_pos = vectorizer.fit_transform(df_pos['verified_reviews'])
+    x_transform_pos = vectorizer.fit_transform(df_pos['text'])
     column_names_pos = vectorizer.get_feature_names_out()
     array_pos = x_transform_pos.toarray()
     token_column_sums_pos = np.sum(array_pos, axis=0)
@@ -32,55 +31,66 @@ def token_weights(df: pd.DataFrame):
     data_pos = {'Tokens': highest_weight_token_names_pos, 'Weights': highest_weight_pos}
     tokens_pos = pd.DataFrame(data_pos)
     sorted_tokens = tokens_pos.sort_values(by=['Weights'], ascending=True)
+    print("\n 10 most important tokens for positive reviews")
     print(tabulate(sorted_tokens, headers='keys', tablefmt='psql'))
     plt.bar(sorted_tokens['Tokens'], sorted_tokens['Weights'])
     plt.xlabel("Tokens")
     plt.ylabel("Weights")
-    plt.xticks(rotation=15)
-    plt.gcf().subplots_adjust(bottom=0.15)
+    plt.xticks(rotation=25)
+    plt.gcf().subplots_adjust(bottom=0.30)
     plt.title("10 most important tokens for positive reviews")
+    plt.show()
+
+
+def token_weights_negative(df: pd.DataFrame):
+    """Show 10 highest token weights for negative reviews"""
+    df_neg = df[df['airline_sentiment'] == "negative"]
+    vectorizer = TfidfVectorizer(tokenizer=text_tokenizer)
+    x_transform_neg = vectorizer.fit_transform(df_neg['text'])
+    column_names_neg = vectorizer.get_feature_names_out()
+    array_neg = x_transform_neg.toarray()
+    token_column_sums_neg = np.sum(array_neg, axis=0)
+    highest_weight_indexes_neg = np.argpartition(token_column_sums_neg, -10)[-10:]
+    highest_weight_token_names_neg = []
+    highest_weight_neg = []
+
+    for index in np.nditer(highest_weight_indexes_neg):
+        highest_weight_token_names_neg.append((column_names_neg[index]))
+        highest_weight_neg.append(token_column_sums_neg[index])
+
+    data_neg = {'Tokens': highest_weight_token_names_neg, 'Weights': highest_weight_neg}
+    tokens_neg = pd.DataFrame(data_neg)
+    sorted_tokens = tokens_neg.sort_values(by=['Weights'], ascending=True)
+    print("\n 10 most important tokens for negative reviews")
+    print(tabulate(sorted_tokens, headers='keys', tablefmt='psql'))
+    plt.bar(sorted_tokens['Tokens'], sorted_tokens['Weights'])
+    plt.xlabel("Tokens")
+    plt.ylabel("Weights")
+    plt.xticks(rotation=25)
+    plt.gcf().subplots_adjust(bottom=0.30)
+    plt.title("10 most important tokens for negative reviews")
     plt.show()
 
 
 def sentiment(df: pd.DataFrame):
     """Sentiment analysis for the amazon alexa review data with Logistic Regression,
     RandomForestClassifier and Support Vector Machine"""
-    x = df['verified_reviews']
-    y = df['sentiment']
+    x = df['text']
+    y = df['airline_sentiment']
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
     vectorizer = CountVectorizer(tokenizer=text_tokenizer)
     x_transform_train = vectorizer.fit_transform(x_train)
     x_transform_test = vectorizer.transform(x_test)
-    lr = LogisticRegression()
-    lr.fit(x_transform_train, y_train)
-    lr_score = lr.score(x_transform_test, y_test)
-    print(f"Logistic regression model prediction accuracy - {lr_score * 100} %")
-    y_pred_lr = lr.predict(x_transform_test)
-    tn, fp, fn, tp = confusion_matrix(y_test, y_pred_lr).ravel()
-    print("Logistic Regression classification report")
-    print(classification_report(y_test, y_pred_lr))
-    """Precision informuje ile przypadków zdiagnozowanych pozytywnie jest rzeczywiscie pozytywna, dla klasy 0 precyzja 
-     wynosi 0.89 a dla klasy 1 0.96, idealny wynik to 1. 
-     Recall informuje o udziale przypadkow zdiagnozowanych pozytywnie wśród wszystkich przypadkow pozytywnych, 
-     rowniez tych zaliczonych do negatywnych (FN). Dla klasy 0 recall wynosi 0.52
-     a dla klasy 1 - 0.99. Niski wynik klasy 0 najprawdopodobniej wynika z bardzo małego udzialu ocen negatywnych w 
-     zbiorze badanych danych, przez co algorytm nie zdołał odpowiednio nauczyć się wychwytawać tego typu przypadków.
-     F1-score to srednia harmoniczna pomiędzy precision a recall, im blizej jest wartosci 1 tym lepszy algorytm 
-     klasyfikujący dla danego przypadku, dla klasy 0, f1 score wynosi 0.66 a niski wynik najprawdopobniej związany jest
-    z wyżej wymienionym powodem. Dla klasy 1 f1-score wynosi 0.98 co jest bardzo dobrym wynikiem.
-    Dodatkowo z analizy wag tokenow w recenzjach negatywnych  i pozytywnych wynikało, że słowa nacechowane negatywnie 
-    niekoniecznie są dominujące w recenzjach negatywnych, co dodatkowo utrudnia rozróżnienie ich od recenzji pozytwnych,
-     z którym dzielone jest wiele słów neutralnych. 
-    """
-    confusion_table = texttable.Texttable()
-    confusion_table.add_rows([["Confusion matrix results", "Number/ratio"],
-                              ["True positives", tp],
-                              ["True negatives", tn],
-                              ["False positives", fp],
-                              ["False negatives", fn],
-                              ["True positives ratio", tp / (tp + fp)],
-                              ["True negatives ratio", tn / (fn + tn)]])
-    print(confusion_table.draw())
+    dt = DecisionTreeClassifier()
+    dt.fit(x_transform_train, y_train)
+    dt_score = dt.score(x_transform_test, y_test)
+    print(f"Decision Tree model prediction accuracy - {dt_score * 100} %")
+    y_pred_dt = dt.predict(x_transform_test)
+    print("Decision Tree classification report")
+    print(classification_report(y_test, y_pred_dt))
+    # y_pred_lr = lr.predict(x_transform_test)
+    # print("Logistic Regression classification report")
+    # print(classification_report(y_test, y_pred_lr))
 
     svml = svm.SVC()
     svml = svml.fit(x_transform_train, y_train)
@@ -88,17 +98,21 @@ def sentiment(df: pd.DataFrame):
     print(f"Support Vector Machine model prediction accuracy - {svml_score * 100} %")
     y_pred_svml = svml.predict(x_transform_test)
     print("Classification report for Support Vector Machine")
-    print(classification_report(y_test, y_pred_svml))  # bardzo niski recall dla klasy 0, odrzucenie algorytmu
-    tn, fp, fn, tp = confusion_matrix(y_test, y_pred_svml).ravel()
-    confusion_table_svml = texttable.Texttable()
-    confusion_table_svml.add_rows([["Confusion matrix results", "Number/ratio"],
-                                   ["True positives", tp],
-                                   ["True negatives", tn],
-                                   ["False positives", fp],
-                                   ["False negatives", fn],
-                                   ["True positives ratio", tp / (tp + fp)],
-                                   ["True negatives ratio", tn / (fn + tn)]])
-    print(confusion_table_svml.draw())
+    print(classification_report(y_test, y_pred_svml))
+    """Klasyfikator przewiduje sentyment tweetow z trafnością ok. 78.5%.
+    Precision informuje jaki odsetek przypadków zdiagnozowanych pozytywnie/neturalnie/negatywnie rzeczywiscie taki jest.
+    Dla recenzji negatywnych precision wynosi 0.82, warto zwrocic takze uwage ze liczba ocen negatywnych jest znaczaco
+    przeważająca. Precision dla ocen neutralnych wynosi 0.63, moze to wynikac z uzycia słow neutralnych, które często
+    będą wykorzystywane także w recenzjach pozytywnych i negatywnych. Precision dla recenzji pozytywnych wynosi 0.78.
+    Poza recenzjami neutralnymi klasyfikator radzi sobie w miare dobrze w klasyfikacji recenzji.
+    Recall informuje o udziale przypadkow zdiagnozowanych pozytwnie/neutralnie/negatywnie wśród wszystkich 
+    takich przypadków, rowniez tych zaliczonych do innych klas (FN). Recall dla klas negatywyny, neutralny, pozytywny
+    wynosi odpowiednio 0.92, 0.49, 0.62. Znów można wyróżnić słaby wynik recenzji neutralnych, jest to 
+    najprawdopodobniej spowodowane wcześniej wymienionymi przyczynami. F1-score to srednia harmoniczna pomiędzy 
+    precision a recall, im blizej jest wartosci 1 tym lepszy algorytm klasyfikujący dla danego przypadku.
+     F1 dla negatywnych wynosi 0.87 jest do dobry wynik, dla neutralnych 0.55 a dla pozytywnych 0.69.
+     Warto zauważyć również, że pomimo najmniejszej liczby recenzji pozytywnych klasyfikator radzi sobie znacznie lepiej
+     z ich wykyrwaniem niż w przypadku recenzji neutralnych."""
 
     rfcl = RandomForestClassifier(n_estimators=150)
     rfcl = rfcl.fit(x_transform_train, y_train)
@@ -106,17 +120,7 @@ def sentiment(df: pd.DataFrame):
     print(f"Random forest classifier prediction accuracy = {rfcl_score * 100} %")
     y_pred_rfcl = rfcl.predict(x_transform_test)
     print("Classification report for Random Forest Classifier")
-    print(classification_report(y_test, y_pred_rfcl))  # recall < 0.5 dla klasy 0, odrzucenie algorytmu
-    tn, fp, fn, tp = confusion_matrix(y_test, y_pred_rfcl).ravel()
-    confusion_table_rfcl = texttable.Texttable()
-    confusion_table_rfcl.add_rows([["Confusion matrix results", "Number/ratio"],
-                                   ["True positives", tp],
-                                   ["True negatives", tn],
-                                   ["False positives", fp],
-                                   ["False negatives", fn],
-                                   ["True positives ratio", tp / (tp + fp)],
-                                   ["True negatives ratio", tn / (fn + tn)]])
-    print(confusion_table_rfcl.draw())
+    print(classification_report(y_test, y_pred_rfcl))
 
 
 def main():
@@ -124,7 +128,8 @@ def main():
     show_plots(df)
     generate_wordclouds(df)
     show_wordclouds()
-    token_weights(df)
+    token_weights_positive(df)
+    token_weights_negative(df)
     sentiment(df)
 
 
--- util.py
+++ util.py
@@ -1,7 +1,6 @@
 import re
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 import glob
 import matplotlib.image
 
@@ -44,58 +43,42 @@ def text_tokenizer(text: str) -> list:
 
 def add_labels(x, y):
     """adding labels to bar chart """
-    for i in range(1,len(x)+1):
-        plt.text(i,y[i-1], y[i-1], ha="center", va="bottom")
+    for i in range(1, len(x) + 1):
+        plt.text(i, y[i - 1], y[i - 1], ha="center", va="bottom")
 
 
 def generate_dataframe() -> pd.DataFrame:
     """Generate a dataframe from a csv file for further use"""
-    df = pd.read_csv('data/amazon_alexa_reviews.csv', sep=';',
-                     usecols=['rating', 'verified_reviews'], encoding='cp1252')
-    df['verified_reviews'].replace(' ', np.NaN, inplace=True)  # replace empty reviews with NaN
-    df.dropna(how='any', inplace=True)  # delete reviews that are Nan
-    df["sentiment"] = df.rating.apply(
-        lambda x: 0 if x in [1, 2] else 1)  # add sentiment column, label reviews < 3 = 0, >=3 = 1
+    df = pd.read_csv('tweets_airline.csv', sep=',',
+                     usecols=['text', 'airline_sentiment'], encoding='utf-8')
     return df
 
 
 def show_plots(df: pd.DataFrame):
     """Generate plots related to dataframe"""
-    ratings = df['rating'].value_counts().sort_index()  # count distribution of review ratings
+    ratings = df['airline_sentiment'].value_counts().sort_index()  # count distribution of review ratings
     plt.bar(ratings.index, ratings.values)
     plt.title(f"Distribution of review ratings")
     plt.xlabel("Review rating score")
     plt.ylabel("Number of reviews")
-    add_labels(ratings.index, ratings.values)
     plt.show()
 
-    sentiments = df['sentiment'].value_counts()  # count distribution of positive and negative sentiment among reviews
-    plt.pie(sentiments.values, shadow=True, labels=["Positive", "Negative"], startangle=90, autopct='%1.1f%%',
-            colors=["Green", "Red"])
-    plt.title("Distribution of positive and negative sentiment reviews")
+    sentiments = df['airline_sentiment'].value_counts()
+    plt.pie(sentiments.values, shadow=True, labels=["Negative", "Neutral", "Positive"],
+            startangle=90, autopct='%1.1f%%',
+            colors=["Red", "Blue", "Green"])
+    plt.title("Distribution of negative, neutral and positve airline reviews")
     plt.show()
 
 
 def generate_wordclouds(df: pd.DataFrame):
     """Generate wordclouds for all reviews, positive only and negative only"""
     stop_list = set(stopwords.words('english'))
-    text_general = " ".join(review for review in df.verified_reviews.astype(str))
+    text_general = " ".join(review for review in df.text.astype(str))
     text_general = cleanup_text(text_general)
     wc = WordCloud(width=2500, height=2500, stopwords=stop_list, background_color='black', colormap='Paired')
     wc.generate(text_general)
     wc.to_file('wordclouds/wc_general.png')
-
-    df_pos = df[df['rating'] >= 3]
-    text_pos = " ".join(review for review in df_pos.verified_reviews.astype(str))
-    text_pos = cleanup_text(text_pos)
-    wc.generate(text_pos)
-    wc.to_file('wordclouds/wc_positive.png')
-
-    df_neg = df[df['rating'] < 3]
-    text_neg = " ".join(review for review in df_neg.verified_reviews.astype(str))
-    text_neg = cleanup_text(text_neg)
-    wc.generate(text_neg)
-    wc.to_file('wordclouds/wc_negative.png')
 
 
 def show_wordclouds():
